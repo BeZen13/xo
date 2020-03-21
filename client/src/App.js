8b5db43787d9cb5ade96b0da4fd2b653f340d49e
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-21 14:59:16 -0600

    adding more

---
+++ client/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Board from './components/Board.js'
+import Squares from './components/Squares.js'
+import Game from './components/Game.js'
+
+export default function App(){
+    
+}
