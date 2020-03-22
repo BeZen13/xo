2ef9223271f1b957f10b4f12c15b048f4a8079d9
Samuel Joseph Betzen <samuelbetzen@gmail.com>
2020-03-21 22:19:34 -0600

    board

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -5,6 +5,22 @@ const initState = {
     squares: Array(9).fill(null)
 }
 
+    export default function Squares(props){
+       const [ square, setSquare ] = useState(initState)
+    
+        return(
+            <button className="square" onClick={() => 
+                setSquare(...prevSquare => ({
+                    ...prevSquare,
+                    value: "X"
+                }))
+            }
+            ></button>
+        )
+    }
+    
+
+
 export default function Board(props){
    const [ fill, setFill ] = useState(initState)
     
--- client/src/components/Squares.js
+++ client/src/components/Squares.js
@@ -1,19 +1,2 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-const initState = {
-    value: null
-}
-
-export default function Squares(props){
-   const [ square, setSquare ] = useState(initState)
-
-    return(
-        <button className="square" onClick={() => 
-            setSquare(...prevSquare => ({
-                ...prevSquare,
-                value: "X"
-            }))
-        }
-        ></button>
-    )
-}
